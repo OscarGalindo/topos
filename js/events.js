29a482b391285156946323f5088eca71c3bbf10c
kaseOga <kaseOga@kaseOga.local>
2015-03-19 16:58:29 +0100

    Evento cada 5 segundos asignando data active a true

--- js/events.js
+++ js/events.js
@@ -1,8 +1,6 @@
 var doc = $(document);
 
 doc.on('nuevoTopo', function () {
-    var noTopo = $('.topo');
-    console.log(noTopo.length);
     console.log('New topo!');
 });
 
--- js/topos.js
+++ js/topos.js
@@ -2,13 +2,18 @@ $(document).ready(function () {
     $('.topo').data('active', false);
 
     var $game = $('#juego'),
+        $topo = $('.topo'),
         $hits = 0,
         $topos = 0;
 
     setInterval(function () {
         if ($topos < 5) {
-            $game.trigger('nuevoTopo');
-            $topos++;
+            var randomDiv = parseInt(Math.random() * $topo.length);
+            if(false === $topo.eq(randomDiv).data('active')) {
+                $game.trigger('nuevoTopo');
+                $topo.eq(randomDiv).data('active', true);
+                $topos++;
+            }
         }
     }, 1000)
 
